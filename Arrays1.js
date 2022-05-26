c638843255226fc0d565f62f39acb6ef4c0c17d0
Gabriela Gonzaga <106291822+gabrielanunesgonzaga@users.noreply.github.com>
2022-05-26 00:58:11 -0300

    Add files via upload

---
+++ Arrays1.js
@@ -0,0 +1,13 @@
+//primeira nota de arrays.
+
+const notas = [10, 6.5, 8, 7.5]
+
+// calculando as medias usando arrays.
+
+let media = (notas [0] + notas [1] + notas[2] + notas[3])
+/ notas.length
+
+//usando o length para percorrer o numero de notas salvo em "notas" e criar
+//uma divisão pelo numero de notas.
+
+console.log(media)
